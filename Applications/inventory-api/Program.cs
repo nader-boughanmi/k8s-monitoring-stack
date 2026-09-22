2cc3a903b0c06f292194ad507cc448a24b18d396
NaderBOUGHANMI <102673609+nader-boughanmi@users.noreply.github.com>
2026-09-22 15:50:01 +0200

    Update Program.cs

--- Applications/inventory-api/Program.cs
+++ Applications/inventory-api/Program.cs
@@ -1,1 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+app.UseHttpsRedirection();
+
+app.MapGet("/servers", () =>
+{
+    return new[]
+    {
+        new
+        {
+            Id = 1,
+            Hostname = "SRV-APP-01",
+            IP = "10.10.10.1",
+            Environment = "PROD"
+        },
+        new
+        {
+            Id = 2,
+            Hostname = "SRV-SQL-01",
+            IP = "10.10.10.20",
+            Environment = "PREPROD"
+        }
+    };
+});
+app.Run();
